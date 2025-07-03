af6e173527d82b2817d68bb812b9bcf5fb32a0bb
Daniel Sperber <github.blurry@9ox.net>
2025-07-03 19:04:36 +0200

    Add test timeout and split, likely jax deadlock

--- __init__.py
+++ __init__.py
@@ -1,14 +1,3 @@
 from ray_utilities.nice_logger import nice_logger
 
 logger = nice_logger(__name__, level="DEBUG")
-
-try:
-    from . import rllib_port
-    from . import args
-except ImportError:
-    import sys
-    import os
-
-    sys.path.append(os.path.dirname(__file__))
-    import rllib_port
-    import args
